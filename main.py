662cb7343fe92dae68c3ad3b95cecbde1b12e993
BelacEr <caleblopezmorgado2007@gmail.com>
2025-08-13 14:31:58 -0600

    PersonaTrack

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ edit_person(person) ➜  Let's the uses modify fields of an existing entry.
 delete_person(people, id) ➜ Removes a person from the list.
 
 """
+import json
+
 from rich import print
 from text_styles import *
 from persona_tracks_module import *
@@ -40,59 +42,7 @@ data = [
 ]
 
 
-import json
 
-"""
-def enter_numbers(message):
-    while True:
-        try:
-            return int(input(message))
-            
-        except ValueError:
-            print("Enter only valid numbers")
-
-def add_person():
-    first_name = input("First name: ")
-    middle_name = input("Midddle name: ")
-    last_name = input("Last name: ")
-    age = enter_numbers("Age: ")
-    languages = input("Languages: ")    # NEED TO CHANGE
-    country = input("Country: ")
-    city = input("City: ")
-    social_media = input("Social media: ")      # NEED TO CHANGE
-
-    new_person = {
-            "First name": first_name,
-            "Middle name"; middle_name,
-            "Last name": last_name,
-            "Age": age,
-            "Languages": languages,     # NEED TO CHANGE
-            "Country": country,    
-            "City": city,
-            "Social media": social_media,   # NEED TO CHANGE
-            }
-
-
-
-def create_data(data):      # create database.json for the first time
-    with open("database.json", "w") as f:
-        json.dump(data, f, indent=4)
-
-def load_data():
-    try:
-        with open("database.json", "r") as file:
-            return json.load(file)  # Returns a list.
-    except FileNotFoundError:
-        return []   # Emtpy list if file doesn't exist
-
-def save_data(data):
-    with open("database.json", "w") as file:
-        json.dump(data, file, indent=2)    # indent for readibility.
-
-if __name__ == '__main__':
-    create_data(data)
-    add_person()
-"""
 
 while True:
     print("\nMenu: (1) Add (2) View (3) Edit (4) Delete (5) Exit")
